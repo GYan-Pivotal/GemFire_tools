333e9bcbb684b1671f420c95432717dac2352308
GYan-Pivotal <gyan@pivotal.io>
2016-07-17 16:33:21 +0800

    Add files via upload

---
+++ filter_stats_from_gfs/filter_stats_from_gfs.py
@@ -0,0 +1,28 @@
+
+import datetime
+import os
+# Using datetime.combine()
+
+#os.system('')
+
+def main():
+    dateStart = datetime.date(2016, 4, 26)
+    tStart = datetime.time(4,40)
+    dtStart=datetime.datetime.combine(dateStart, tStart)
+    dateEnd = datetime.date(2016, 4, 27)
+    tEnd= datetime.time(2,0)
+    dtEnd=datetime.datetime.combine(dateEnd, tEnd)
+    dateInit = dtStart
+    for i in range((dtEnd - dtStart).seconds):
+        day = dtStart + datetime.timedelta(seconds=i)
+        if (i%60 == 0):
+            strDateFrom= dateInit.strftime( '%Y/%m/%d %H:%M:%S' )+".000 CST"
+            strDate= day.strftime( '%Y/%m/%d %H:%M:%S' )+".000 CST"
+            #print 'gemfire stats -starttime="'+strDateFrom+'" -endtime="'+strDate+'" -archive=statics_p.gfs|grep cpuActive >> aa.txt'
+            os.system('gemfire stats -starttime="'+strDateFrom+'" -endtime="'+strDate+'" -archive=statics_p.gfs|grep cpuActive >> aa.txt')
+            dateInit=day
+            #print strDate
+
+
+if __name__ == '__main__':
+    main()
